b3c25097ced3af0b4bf760f9662bb8da8e35e0f3
Dean <dhauptflesh@gmail.com>
2022-10-24 16:39:35 +0200

    requiremnts update

---
+++ main.py
@@ -0,0 +1,61 @@
+#read https://pypi.org/project/SpeechRecognition/
+# https://pypi.org/project/PyAudio/
+# https://pypi.org/project/pywhatkit/
+# https://pypi.org/project/wikipedia/
+# https://pypi.org/project/pyjokes/
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            comand = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print (command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person =  command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say that again.')
+
+
+while True:
+    run_alexa()
